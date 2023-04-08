d993f0349fdb44c19c2ab5e4aeda297c63605ae3
Jun <kumejun2000@gmail.com>
2023-04-09 00:31:16 +0900

    sim.run()のバグ修正

--- simulator.py
+++ simulator.py
@@ -11,6 +11,7 @@ class Simulation:
     def run(self):
         policy = DDQN()
         for s in range(self.sim):
+            policy.reset()
             for e in range(self.epi):
                 state = self.env.reset()[0]
                 terminated, truncated = False, False
@@ -19,4 +20,4 @@ class Simulation:
                     next_state, reward, terminated, truncated, info = self.env.step(action)
                     policy.update(state, action, reward, next_state, (terminated or truncated))
                     state = next_state
-        env.close()
+        self.env.close()
