d761923afd078a559f75f1f46dc9f08a3feb9ded
Jun <kumejun2000@gmail.com>
2023-04-09 00:07:38 +0900

    dqnに対応するように変更

--- simulator.py
+++ simulator.py
@@ -1,9 +1,15 @@
+from policy.dqn import DQN
+
 def simulation(sims, epis, env):
+    policy = DQN()
     for sim in range(sims):
+        policy.reset()
         for epi in range(epis):
             state = env.reset()[0]
             terminated, truncated = False, False
             while not (terminated or truncated):
-                action = env.action_space.sample()
+                action = policy.action(state)
                 next_state, reward, terminated, truncated, info = env.step(action)
+                policy.update(state, action, reward, next_state, (terminated or truncated))
+                state = next_state
     env.close()
