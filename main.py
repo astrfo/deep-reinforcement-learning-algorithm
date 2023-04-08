a47fc532526bac6fa19ab36f8cae1c5557d24dc3
Jun <kumejun2000@gmail.com>
2023-04-09 01:11:04 +0900

    humanからrgb_arrayに変更

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from simulator import Simulation
 
 if __name__ == '__main__':
     sim = 1
-    epi = 10
-    env = gym.make('CartPole-v1', render_mode='human').unwrapped
+    epi = 500
+    env = gym.make('CartPole-v1', render_mode='rgb_array').unwrapped
 
     simulation = Simulation(sim, epi, env)
     simulation.run()
