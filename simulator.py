c4dd934029885372676d313621c3c3b91675739d
Jun <kumejun2000@gmail.com>
2023-04-09 00:38:36 +0900

    simulatorにcollectorを追加

--- simulator.py
+++ simulator.py
@@ -1,5 +1,6 @@
 from policy.dqn import DQN
 from policy.ddqn import DDQN
+from collector import Collector
 
 
 class Simulation:
@@ -7,17 +8,23 @@ class Simulation:
         self.sim = sim
         self.epi = epi
         self.env = env
+        self.collector = Collector(sim, epi)
+        self.policy = DDQN()
 
     def run(self):
-        policy = DDQN()
         for s in range(self.sim):
-            policy.reset()
+            self.collector.reset()
+            self.policy.reset()
             for e in range(self.epi):
                 state = self.env.reset()[0]
-                terminated, truncated = False, False
+                terminated, truncated, total_reward = False, False, 0
                 while not (terminated or truncated):
-                    action = policy.action(state)
+                    action = self.policy.action(state)
                     next_state, reward, terminated, truncated, info = self.env.step(action)
-                    policy.update(state, action, reward, next_state, (terminated or truncated))
+                    self.policy.update(state, action, reward, next_state, (terminated or truncated))
                     state = next_state
+                    total_reward += reward
+                self.collector.collect_episode_data(total_reward)
+            self.collector.save_episode_data()
+        self.collector.save_simulation_data()
         self.env.close()
