1a36451d4f172b9131f0d8e91741785c328865a0
Jun <kumejun2000@gmail.com>
2023-04-11 15:38:15 +0900

    device等の修正

--- policy/policygradient.py
+++ policy/policygradient.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.distributions import Categorical
 
 
 class PolicyGradient:
@@ -11,20 +12,24 @@ class PolicyGradient:
         self.gamma = 0.98
         self.device = torch.device('cpu')
         self.model = PolicyNet(input_size=4, hidden_size=128, output_size=2)
-        self.model.to(self.device)
+        # self.model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.alpha)
         self.memory = []
 
     def reset(self):
         self.model = PolicyNet(input_size=4, hidden_size=128, output_size=2)
-        self.model.to(self.device)
+        # self.model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.alpha)
 
     def action(self, state):
-        s = torch.tensor(state, dtype=torch.float32).to(self.device).unsqueeze(0)
-        with torch.no_grad():
-            prob = self.model(s).squeeze().to('cpu').detach().numpy().copy()
-        action = np.random.choice(2, p=prob)
+        # s = torch.tensor(state, dtype=torch.float32).to(self.device).unsqueeze(0)
+        # with torch.no_grad():
+            # prob = self.model(s).squeeze().to('cpu').detach().numpy().copy()
+        # action = np.random.choice(2, p=prob)
+        s = torch.tensor(state[np.newaxis, :])
+        prob = self.model(s)
+        prob = prob[0]
+        action = Categorical(prob).sample().item()
         return action, prob[action]
 
     def add(self, reward, prob):
@@ -32,13 +37,14 @@ class PolicyGradient:
         self.memory.append(data)
 
     def update(self):
-        G, loss = 0, 0
+        G_tau, loss = 0, 0
         for r, p in reversed(self.memory):
-            G = r + self.gamma * G
+            G_tau = r + self.gamma * G_tau
         
         for r, p in self.memory:
-            loss += -np.log(p) * G
-        loss = torch.tensor(loss, dtype=torch.float32, requires_grad=True).to(self.device)
+            # loss += -np.log(p) * G_tau
+            loss += -torch.log(p) * G_tau
+        # loss = torch.tensor(loss, dtype=torch.float32, requires_grad=True).to(self.device)
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -52,11 +58,10 @@ class PolicyNet(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = self.softmax(x)
+        x = F.softmax(x, dim=1)
         return x
