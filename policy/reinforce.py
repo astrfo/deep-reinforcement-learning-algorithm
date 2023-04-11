e72ed313ca345c508d2e412e3bd4f39be5d7cf21
Jun <kumejun2000@gmail.com>
2023-04-11 16:15:12 +0900

    方策勾配法の変更に伴い，REINFORCEも同様の変更

--- policy/reinforce.py
+++ policy/reinforce.py
@@ -3,64 +3,43 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.distributions import Categorical
 
 
 class REINFORCE:
     def __init__(self):
-        self.alpha = 0.01
+        self.alpha = 0.0002
         self.gamma = 0.98
         self.device = torch.device('cpu')
         self.model = PolicyNet(input_size=4, hidden_size=128, output_size=2)
-        self.model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.alpha)
+        self.memory = []
 
     def reset(self):
         self.model = PolicyNet(input_size=4, hidden_size=128, output_size=2)
-        self.model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.alpha)
 
-    def episode_reset(self):
-        self.states = []
-        self.actions = []
-        self.rewards = []
-
     def action(self, state):
-        s = torch.tensor(state, dtype=torch.float32).to(self.device).unsqueeze(0)
-        with torch.no_grad():
-            pi = self.model(s)
-        action = torch.multinomial(pi, num_samples=1).item()
-        return action
+        s = torch.tensor(state[np.newaxis, :])
+        prob = self.model(s)
+        prob = prob[0]
+        action = Categorical(prob).sample().item()
+        return action, prob[action]
 
-    def update(self, state, action, reward, next_state, done):
-        self.states.append(state)
-        self.actions.append(action)
-        self.rewards.append(reward)
+    def add(self, reward, prob):
+        data = (reward, prob)
+        self.memory.append(data)
 
-        if not done:
-            return
-        
-        states = torch.tensor(np.array(self.states), dtype=torch.float32).to(self.device)
-        pi = self.model(states)
-        selected_log_probs = torch.log(pi.gather(1, torch.tensor(self.actions).view(-1, 1).to(self.device)))
-
-        discounted_rewards = []
-        for t in range(len(self.rewards)):
-            Gt = 0
-            for r, step_reward in enumerate(self.rewards[t:]):
-                Gt += step_reward * (self.gamma ** r)
-            discounted_rewards.append(Gt)
-
-        discounted_rewards = torch.tensor(discounted_rewards).to(self.device)
-
-        loss = []
-        for log_prob, Gt in zip(selected_log_probs, discounted_rewards):
-            loss.append(-log_prob * Gt)
-
-        loss = torch.cat(loss).sum()
+    def update(self):
+        Gt, loss = 0, 0
+        for r, p in reversed(self.memory):
+            Gt = r + self.gamma * Gt
+            loss += -torch.log(p) * Gt
 
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+        self.memory = []
 
 
 class PolicyNet(nn.Module):
@@ -69,11 +48,10 @@ class PolicyNet(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = self.softmax(x)
+        x = F.softmax(x, dim=1)
         return x
