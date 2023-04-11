7cfaaa9cbdde7b6748a9c2b0ec7f5b20b9c46d67
Jun <kumejun2000@gmail.com>
2023-04-11 16:16:04 +0900

    PGSimulationのコメント文変更

--- simulator.py
+++ simulator.py
@@ -5,6 +5,7 @@ from policy.duelingdqn import DuelingDQN
 from policy.categoricaldqn import CategoricalDQN
 from policy.prioritizedreplaybuffer import DQN_PER
 from policy.policygradient import PolicyGradient
+from policy.reinforce import REINFORCE
 from collector import Collector
 
 
@@ -37,16 +38,15 @@ class Simulation:
 
 class PGSimulation:
     """
-    方策勾配法では軌跡を必要とするため(Gtが必要)エピソード始めに
-    `episode_reset()`メソッドを用いて軌跡を保存する配列を用意する
-    とりあえずの妥協案として別クラスPGSimulationを用意した
+    方策勾配法では収益Gを必要とするため
+    self.policy.add(reward, prob)を用意した
     """
     def __init__(self, sim, epi, env):
         self.sim = sim
         self.epi = epi
         self.env = env
         self.collector = Collector(sim, epi)
-        self.policy = PolicyGradient()
+        self.policy = REINFORCE()
 
     def run(self):
         for s in range(self.sim):
